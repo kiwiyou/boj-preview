f82f911966950e278aa272522740ea0800821b0b
kiwiyou <kiwiyou@kiwiyou.dev>
2025-05-15 23:04:18 +0900

    biome

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
-import { Hono } from 'hono/tiny'
+import { Hono } from "hono/tiny";
 
-import { isbot } from 'isbot'
-import { querySingle } from './vendor/solved'
+import { isbot } from "isbot";
+import { querySingle } from "./vendor/solved";
 
 const getPath = (req: Request) => {
-  const userAgent = req.headers.get('User-Agent')
-  const isBot = isbot(userAgent) ? 'is-bot' : 'not-bot'
-  const path = req.url.replace(/^https?:\/\/[^/]+(\/[^?]*).*/, '$1')
-  return `/${isBot}${path}`
-}
+	const userAgent = req.headers.get("User-Agent");
+	const isBot = isbot(userAgent) ? "is-bot" : "not-bot";
+	const path = req.url.replace(/^https?:\/\/[^/]+(\/[^?]*).*/, "$1");
+	return `/${isBot}${path}`;
+};
 
 const app = new Hono({
-  getPath,
-})
+	getPath,
+});
 
-app.on('GET', '/not-bot', (c) => c.redirect('https://www.acmicpc.net/', 301))
-app.on('GET', ['/not-bot/:id', '/not-bot/problem/:id'], (c) => c.redirect(`https://www.acmicpc.net/problem/${c.req.param('id')}`, 301))
-app.on('GET', ['/not-bot/u/:handle', '/not-bot/user/:handle'], (c) => c.redirect(`https://www.acmicpc.net/user/${c.req.param('handle')}`, 301))
-app.on('GET', ['/not-bot/s/:id', '/not-bot/source/:id'], (c) => c.redirect(`https://www.acmicpc.net/source/${c.req.param('id')}`, 301))
-app.on('GET', ['/not-bot/q/:query', '/not-bot/query/:query'], async (c) => {
-  const query = c.req.param('query')
-  const problem = await querySingle(query)
-  if (!problem) {
-    return c.text('Not found', 404)
-  }
-  return c.redirect(`https://www.acmicpc.net/problem/${problem.problemId}`, 302)
-})
+app.on("GET", "/not-bot", (c) => c.redirect("https://www.acmicpc.net/", 301));
+app.on("GET", ["/not-bot/:id", "/not-bot/problem/:id"], (c) =>
+	c.redirect(`https://www.acmicpc.net/problem/${c.req.param("id")}`, 301),
+);
+app.on("GET", ["/not-bot/u/:handle", "/not-bot/user/:handle"], (c) =>
+	c.redirect(`https://www.acmicpc.net/user/${c.req.param("handle")}`, 301),
+);
+app.on("GET", ["/not-bot/s/:id", "/not-bot/source/:id"], (c) =>
+	c.redirect(`https://www.acmicpc.net/source/${c.req.param("id")}`, 301),
+);
+app.on("GET", ["/not-bot/q/:query", "/not-bot/query/:query"], async (c) => {
+	const query = c.req.param("query");
+	const problem = await querySingle(query);
+	if (!problem) {
+		return c.notFound();
+	}
+	return c.redirect(
+		`https://www.acmicpc.net/problem/${problem.problemId}`,
+		302,
+	);
+});
 
-export default app
+export default app;
--- src/vendor/solved.ts
+++ src/vendor/solved.ts
@@ -1,19 +1,23 @@
-export async function querySingle(query: string): Promise<SearchProblem | null> {
-    try {
-        const req = await fetch(`https://solved.ac/api/v3/search/problem?query=${encodeURIComponent(query)}`)
-        const problems: Resources<SearchProblem> = await req.json()
-        return problems.items[0]
-    } catch (e) {
-        console.error(e)
-        return null
-    }
+export async function querySingle(
+	query: string,
+): Promise<SearchProblem | null> {
+	try {
+		const req = await fetch(
+			`https://solved.ac/api/v3/search/problem?query=${encodeURIComponent(query)}`,
+		);
+		const problems: Resources<SearchProblem> = await req.json();
+		return problems.items[0];
+	} catch (e) {
+		console.error(e);
+		return null;
+	}
 }
 
 export type Resources<Item> = {
-    count: number
-    items: Item[]
-}
+	count: number;
+	items: Item[];
+};
 
 export type SearchProblem = {
-    problemId: number
-}
+	problemId: number;
+};
